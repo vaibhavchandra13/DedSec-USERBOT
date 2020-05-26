7bad9ac36103d6750c541c6e713cd61c9f9e6dd3
Aid3n_Pearce <61540835+Aid-3n@users.noreply.github.com>
2020-05-26 18:48:08 +0530

    Add files via upload

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,20 @@
+"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
+
+#@command(outgoing=True, pattern="^.alive$")
+@borg.on(admin_cmd(pattern=r"alive"))
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.edit("`I am NOT DED😎 \nI Have No Issues Till Now\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
+                     f"`My owner`: {DEFAULTUSER}\n"
+                     "`\nPython: 3.7.3\nFork by:` @Aid_3n \n"
+                     "`Database Status: Awesome🎖️\n\nLet's Try Some Plugins🕶️\n`"
+                     "[Deploy this userbot Now](https://github.com/Aiden232/US3RB0T)")

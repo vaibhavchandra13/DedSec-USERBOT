57d925c214ba5b9b4691f6146ac17a02f6f297bb
Aid3n_Pearce <61540835+Aid-3n@users.noreply.github.com>
2020-05-28 15:37:24 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -36,7 +36,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = "Get your own userbot, don't use another's\n ib @Aid_3n for learning how to get a userbot!"
+            reply_pop_up_alert = "Why Are You Touching That Button,Mate? Get Your Own Userbot NOW For Free😂"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
